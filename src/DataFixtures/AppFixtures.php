2ff31dc940b0e4f906bcf29540c166ff8d8f9340
Kboucaul <kboucaul@gmail.com>
2020-08-12 20:29:44 +0200

    Il est desormais possible de créer un utilisateur enregistré dans la bdd, de lui proposer de s'inscrire, de se connecter ou de se connecter, il peut desormais modifier ses informations ainsi que son mot de passe via un formulaire spécial, les données sont protegees, la prochaine étape sera d'integrer ces utilisateurs au site, avec une bio affichable dans l'annonce

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,30 +4,65 @@ namespace App\DataFixtures;
 
 use App\Entity\Ad;
 use Faker\Factory;
+use App\Entity\User;
 use App\Entity\Image;
+use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class AppFixtures extends Fixture
 {
+    private $encoder;
+    public function __construct(UserPasswordEncoderInterface $encoder)
+    {
+        $this->encoder = $encoder;
+    }
     public function load(ObjectManager $manager)
     {
         $faker = Factory::create('FR-fr');
-        for($i = 1; $i <= 30; $i++)
+        $users = [];
+        $genres = ['men', 'women'];
+        //nous gerons les utilisateurs
+        for($i = 1; $i <= 10; $i++)
+        {
+            $user = new User();
+
+            $genre = $faker->randomElement($genres);
+
+            $picture = "https://randomuser.me/api/portraits/";
+            $pictureId = $faker->numberBetween(1, 99) . '.jpg';
+            $picture = $picture.$genre.'/'.$pictureId;
+
+            $hash = $this->encoder->encodePassword($user, 'password');
+
+            $user->setFirstName($faker->firstname($genre))
+                 ->setLastName($faker->lastname)
+                 ->setEmail($faker->email)
+                 ->setIntroduction($faker->sentence())
+                 ->setDescription('<p>'.join('</p><p>', $faker->paragraphs(3)). '</p>')
+                 ->setHash($hash)
+                 ->setPicture($picture);
+            $manager->persist($user);
+            $users[] = $user;  
+        }
+        //nous gerons les annonces
+        for($i = 1; $i <= 10; $i++)
         {
             $title = $faker->sentence();
             $coverImage = $faker->imageUrl(1000, 350); //(hauteur/largeur)
             $introduction = $faker->paragraph(2);
             $content = '<p>'.join('</p><p>', $faker->paragraphs(5)). '</p>';
 
-
+            $user = $users[mt_rand(0, count($users) - 1)];
             $ad = new Ad();
             $ad->setTitle($title)
                 ->setCoverImage($coverImage)
                 ->setIntroduction($introduction)
                 ->setContent($content)
                 ->setPrice(mt_rand(40, 200))
-                ->setRooms(mt_rand(1, 5));
+                ->setRooms(mt_rand(1, 5))
+                ->setAuthor($user);
 
 
             for ($j = 1; $j <= mt_rand(2, 5); $j++)
--- src/Entity/Ad.php
+++ src/Entity/Ad.php
@@ -7,7 +7,6 @@ use App\Repository\AdRepository;
 use Doctrine\ORM\Mapping as ORM;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\Common\Collections\ArrayCollection;
-
 use Symfony\Component\Validator\Constraints as Assert;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
@@ -72,6 +71,12 @@ class Ad
      * @Assert\Valid()
      */
     private $images;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=User::class, inversedBy="ads")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $author;
 
     public function __construct()
     {
@@ -209,6 +214,18 @@ class Ad
                 $image->setAd(null);
             }
         }
+
+        return $this;
+    }
+
+    public function getAuthor(): ?User
+    {
+        return $this->author;
+    }
+
+    public function setAuthor(?User $author): self
+    {
+        $this->author = $author;
 
         return $this;
     }
